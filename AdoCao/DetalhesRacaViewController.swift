09e1c16ee398c4fecedc10ba7e7246e97c09409b
marcosvsSampaio <vinicius.marcos.mvvlk@gmail.com>
2022-06-08 19:12:30 -0300

    Cria telas Detalhes de Raca e Cadastrar Amigo

---
+++ AdoCao/DetalhesRacaViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetalhesRacaViewController.swift
+//  AdoCao
+//
+//  Created by Marcos Vinicius on 08/06/22.
+//
+
+import UIKit
+
+class DetalhesRacaViewController: UIViewController {
+
+    @IBOutlet weak var carcteristicasLabel: UILabel!
+    @IBOutlet weak var naturalidadeLabel: UILabel!
+    @IBOutlet weak var pesoLabel: UILabel!
+    @IBOutlet weak var alturaLabel: UILabel!
+    @IBOutlet weak var estimativaDeVidaLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
